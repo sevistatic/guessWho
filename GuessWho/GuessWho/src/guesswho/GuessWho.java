8a88a49674b974fb3af1e1b6cd1e37f07097b94a
Spencer Bryant <bryants620@gmail.com>
2017-04-21 04:11:16 -0400

    cleaning up guesswho and background.java

--- GuessWho/GuessWho/src/guesswho/GuessWho.java
+++ GuessWho/GuessWho/src/guesswho/GuessWho.java
@@ -1,9 +1,9 @@
 package guesswho;
 import java.awt.*;
 import javax.swing.*;
+
 /**
- *
- * @author Spencer
+ * @author Spencer Bryant
  */
 public class GuessWho {
 static final int START_TIME = 99;
@@ -11,31 +11,42 @@ static int systemStartTime;
 static String startInfo;
 static final boolean PARTIAL_INFO = false;
 static double startTime;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         JFrame gameFrame = new JFrame("Spencer's Guess Who Game");
+
+        //Set up game window
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         int height = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
         int width = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
         gameFrame.setSize(width, height);
+
         try{
+        //adds game objects to game window
         Background background = new Background(gameFrame.getWidth(), gameFrame.getHeight());
         BorderLayout g = new BorderLayout();
         background.setLayout(g);
         background.init();
         gameFrame.add(background);
         gameFrame.setVisible(true);
+
+        //set start time for logging
         systemStartTime = (int)(System.currentTimeMillis() / 1000);
-        //background.setup();
+
+        //exit flag
         boolean b = false;
+
+        //main game loop
         do{
             pause(40);
-            //background.updateTimer(START_TIME - ((int)(System.currentTimeMillis() / 1000) - systemStartTime));
             background.repaint();
+
             if (!b){
-                 JOptionPane.showMessageDialog (null, 
+                //rules dialog
+                JOptionPane.showMessageDialog (null,
                 "Guess the character the computer chose by asking questions about the character's features using the bar at the left and the \"ASK\" button."
                 + "\nEach time you ask, 10 points will be deducted from your score and if you guess the correct character, your current score will be added to a running total."
                 + "\nYou get bonus points for using clever strategies to come up with the answer in only a few guesses."
@@ -43,24 +54,26 @@ static double startTime;
                 + "\nClick the \"CARD INFO\" button to learn which features a certain character has."
                 + "\nLeft-click to select a character and right-click to turn them over."
                 );
-                 Background.outFile.println("Begin Logging");
-                 startTime = Background.currentTime();
-                 Background.outFile.printf("Participant Start Time: %.3f", startTime);
-                 Background.outFile.println();
-                 Background.outFile.printf("Trial Start Time: %.3f", startTime);
-                 Background.outFile.println();
-                 Background.outFile.println("Trial Number: " + Background.getTrialNum());
-                 Background.outFile.println("----------------------------------------");
-                 Background.outFile.flush();
-                 b = true;
-        Background.outFile.flush();
+
+                //begin logging trials
+                Background.outFile.println("Begin Logging");
+                startTime = Background.currentTime();
+                Background.outFile.printf("Participant Start Time: %.3f", startTime);
+                Background.outFile.println();
+                Background.outFile.printf("Trial Start Time: %.3f", startTime);
+                Background.outFile.println();
+                Background.outFile.println("Trial Number: " + Background.getTrialNum());
+                Background.outFile.println("----------------------------------------");
+                Background.outFile.flush();
+                b = true;
+                Background.outFile.flush();
             }
-        } while(b);
+        } while(b);//exit flag
         } catch (Exception e){
             System.err.println(e.getMessage());
         }
     }//end method main
-        
+
     /**
      * Pauses the game for a specific amount of time (in ms)
      * @param ms the number of milliseconds to pause the game
@@ -73,7 +86,3 @@ static double startTime;
         }
     } // end method pause
 }//end class Main
-//to do: log @mouse click: mouse location, object clicked, time
-//to do: log @trial end: total trial time
-//to do: have drop boxes color, but not disable to reflect a previous choice.
-//to do: implement proper radio button use
