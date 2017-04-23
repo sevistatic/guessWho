484aba81c7755531f136204a1cb924ba2ec223f7
Spencer Bryant <bryants620@gmail.com>
2017-04-23 14:07:57 -0400

    make types of features into their own class

--- src/guesswho/Feature.java
+++ src/guesswho/Feature.java
@@ -1,36 +1,92 @@
 package guesswho;
 /**
- * 
+ *
  * @author Spencer
  *Used entirely to describe the features used in the JLists found in Background.java.
  *An example of a feature is sex.  It has a parity of 2, because there can be a boy or a girl.
  *boy and  girl would be the options in this case.
  */
 class Feature {
+  private String name;//name: beard
+  private int parity;//parity: 2
+  private String[] options;//options = {yes, no}
+  Feature(String n, String[] o){
+      parity = o.length;
+      name = n;
+      options = o;
+  }
 
-//name: beard
-//parity: 2
-//options = {true, false}
-    private String name;
-    private int parity;
-    private String[] options;
-    Feature(String n, String[] o){
-        parity = o.length;
-        name = n;
-        options = o;
-    }
+  public String getName() {
+      return name;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String[] getOptions() {
+      return options;
+  }
 
-    public String[] getOptions() {
-        return options;
-    }
-
-    public int getParity() {
-        return parity;
-    }
-    
-    
+  public int getParity() {
+      return parity;
+  }
+}
+//-----------------------------------------------------------------------------
+class SkinColorFeature extends Feature {
+  SkinColorFeature(){
+    super("Skin Color", new String[]{"light skin", "dark skin"});
+  }
+}
+class EyeColorFeature extends Feature{
+  EyeColorFeature(){
+    super("Eye Color", new String[]{"blue eyes", "black eyes", "brown eyes", "green eyes", "grey eyes"});
+  }
+}
+class SexFeature extends Feature {
+  SexFeature(){
+    super("Sex", new String[]{"boy", "girl"});
+  }
+}
+class MouthShapeFeature extends Feature{
+  MouthShapeFeature(){
+    super("Mouth", new String[]{"smiling", "frowning"});
+  }
+}
+class LipsFeature extends Feature {
+  LipsFeature(){
+    super("Lips", new String[]{"big lips", "thin lips"});
+  }
+}
+class HairFeature extends Feature{
+  HairFeature(){
+    super("Hair", new String[]{"blonde hair", "black hair", "brown hair", "red hair", "grey hair", "bald"});
+  }
+}
+class BeardFeature extends Feature {
+  BeardFeature(){
+    super("Beard", new String[]{"beard", "no beard"});
+  }
+}
+class MustacheFeature extends Feature{
+  MustacheFeature(){
+    super("Mustache", new String[]{"mustache", "no mustache"});
+  }
+}
+class NoseFeature extends Feature {
+  NoseFeature(){
+    super("Nose", new String[]{"big nose", "short nose", "thin nose"});
+  }
+}
+class ShirtFeature extends Feature{
+  ShirtFeature(){
+    super("Shirt", new String[]{"blue shirt", "black shirt", "red shirt", "green shirt", "orange shirt",
+									"yellow shirt", "purple shirt"/*, "white shirt", "leopard shirt", "warning shirt"*/});
+  }
+}
+class HatFeature extends Feature {
+  HatFeature(){
+    super("Headwear", new String[]{"hat", "no hat"});
+  }
+}
+class EyewearFeature extends Feature{
+  EyewearFeature(){
+    super("Eyewear", new String[]{"glasses", "no glasses"});
+  }
 }
--- src/guesswho/FeaturesBar.java
+++ src/guesswho/FeaturesBar.java
@@ -10,6 +10,8 @@ class FeaturesBar extends JPanel{
   public ArrayList<Feature> featuresSet;
   public static ArrayList<JList> jListList;
   private final int BORDER_SIZE = 2;
+  private final int NUM_OF_FEATURE_COLS = 3;
+  private final int NUM_OF_FEATURE_ROWS = 4;
 
 	private static MouseHandler mouseHandler;
 
@@ -18,6 +20,8 @@ class FeaturesBar extends JPanel{
     featuresSet = new ArrayList<Feature>();
 		jListList = new ArrayList<JList>();
 		buildFeatures();
+		setLayout(new GridLayout(NUM_OF_FEATURE_ROWS, NUM_OF_FEATURE_COLS));
+		setBorder(new LineBorder(Color.black, BORDER_SIZE));
 		initFeaturesBar();
 	}
 
@@ -26,34 +30,35 @@ class FeaturesBar extends JPanel{
 	}
 
 	public void buildFeatures() {
-		featuresSet.add(new Feature("Skin Color", new String[]{"light skin", "dark skin"}));
-		featuresSet.add(new Feature("Eye Color", new String[]{"blue eyes", "black eyes", "brown eyes", "green eyes", "grey eyes"}));
-		featuresSet.add(new Feature("Sex", new String[]{"boy", "girl"}));
-		featuresSet.add(new Feature("Mouth", new String[]{"smiling", "frowning"}));
-		featuresSet.add(new Feature("Lips", new String[]{"big lips", "thin lips"}));
-		featuresSet.add(new Feature("Hair", new String[]{"blonde hair", "black hair", "brown hair", "red hair", "grey hair", "bald"}));
-		featuresSet.add(new Feature("Beard", new String[]{"beard", "no beard"}));
-		featuresSet.add(new Feature("Mustache", new String[]{"mustache", "no mustache"}));
-		featuresSet.add(new Feature("Nose", new String[]{"big nose", "short nose", "thin nose"}));
-		featuresSet.add(new Feature("Shirt", new String[]{"blue shirt", "black shirt", "red shirt", "green shirt", "orange shirt",
-									"yellow shirt", "purple shirt"/*, "white shirt", "leopard shirt", "warning shirt"*/}));
-		featuresSet.add(new Feature("Headwear", new String[]{"hat", "no hat"}));
-		featuresSet.add(new Feature("Eyewear", new String[]{"glasses", "no glasses"}));
+		featuresSet.add(new SkinColorFeature());
+		featuresSet.add(new EyeColorFeature());
+		featuresSet.add(new SexFeature());
+		featuresSet.add(new MouthShapeFeature());
+		featuresSet.add(new LipsFeature());
+		featuresSet.add(new HairFeature());
+		featuresSet.add(new BeardFeature());
+		featuresSet.add(new MustacheFeature());
+		featuresSet.add(new NoseFeature());
+		featuresSet.add(new ShirtFeature());
+		featuresSet.add(new HatFeature());
+		featuresSet.add(new EyewearFeature());
 	}
 
 	public void initFeaturesBar() {
-		setLayout(new GridLayout(4, 2));
-		setBorder(new LineBorder(Color.black, BORDER_SIZE));
-		for (int i = 0; i < featuresSet.size(); i++) {
-			JPanel j = new JPanel();
-			j.setLayout(new GridLayout(1, 1));
-			String[] data = featuresSet.get(i).getOptions();
-			GuessList a = new GuessList(data, featuresSet.get(i).getName());
-			a.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-			jListList.add(a);
-			a.addMouseListener(mouseHandler);
-			j.add(jListList.get(i));
-			add(j);
+		for (int feature = 0; feature < featuresSet.size(); feature++) {
+			JPanel featurePanel = new JPanel();
+			featurePanel.setLayout(new GridLayout(1, 1));
+
+      Feature currentFeature = featuresSet.get(feature);
+			GuessList currentFeatureSelector = new GuessList(
+        currentFeature.getOptions(), currentFeature.getName()
+      );
+			currentFeatureSelector.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+			jListList.add(currentFeatureSelector);
+			currentFeatureSelector.addMouseListener(mouseHandler);
+			featurePanel.add(currentFeatureSelector);
+			add(featurePanel);
 		}
 	}
 
